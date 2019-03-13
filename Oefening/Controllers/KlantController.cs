8cbafe13069af431be982ce674c4124331388e57
c1042450 <c1042450@G-D203-14>
2019-03-13 14:08:30 +0100

    this is a commit

--- Oefening/Controllers/KlantController.cs
+++ Oefening/Controllers/KlantController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Oefening.Models;
 
 namespace Oefening.Controllers
 {
@@ -11,7 +12,7 @@ namespace Oefening.Controllers
         public IActionResult Index()
         {
             //Get list of klanten
-            List<Klant>
+            List<Klant> vKlanten = new List<Klant>
         }
     }
 }
--- Oefening/Models/Klant.cs
+++ Oefening/Models/Klant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,7 +12,7 @@ namespace Oefening.Models
         public string Naam { get; set; }
         public string Voornaam { get; set; }
 
-        
+        [DataType(DataType.Date)]
         public DateTime AangemaaktDatum { get; set; }
     }
 }
